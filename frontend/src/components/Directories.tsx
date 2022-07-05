872db65592bcc0a1a4da6bf541c41dadb52b5242
SummaryPuppet <adrianm_sv@hotmail.com>
2022-07-05 18:58:50 +0000

    move Make directory button

--- frontend/src/components/Directories.tsx
+++ frontend/src/components/Directories.tsx
@@ -5,8 +5,6 @@ import api from "./api/api"
 
 import Directory from "./Directory"
 import DropdownMenu from "./DropdownMenu"
-import OpenModal from "./OpenModal"
-import MakeDirectory from "./MakeDirectory"
 
 const Directories = ()=>{
   const [directories, setDirectories] = useState([])
@@ -36,16 +34,10 @@ const Directories = ()=>{
     isPath()
   }, [params])
 
-  const style = "text-center text-lg px-20 py-2 border-2 border-gray-400 rounded-md mb-3 flex row-start-2 lg:flex-row dark:text-white dark:border-gray-500 dark:bg-gray-900"
+  const style = "text-center text-lg px-20 py-2 border-2 border-gray-400 rounded-md mb-3 flex lg:flex-row dark:text-white dark:border-gray-500 dark:bg-gray-900"
 
   return (
     <main className="sm:flex sm:flex-col md:grid md:grid-cols-3 xl:grid xl:grid-cols-5 grid-rows-5 place-items-stretch gap-3" >
-      <OpenModal 
-        buttonName="Make Directory" 
-        buttonStyle="mb-3 px-20 py-2 border-2 border-emerald-500 rounded-md text-2xl bg-emerald-500 xl:bg-transparent hover:bg-emerald-500 dark:text-white" 
-      >
-        <MakeDirectory />
-      </OpenModal>
         {
           directories.map((directory, index) => (
             <div className={style} key={index}>
--- frontend/src/components/HeaderMenu.tsx
+++ frontend/src/components/HeaderMenu.tsx
@@ -2,17 +2,26 @@ import OpenModal from "./OpenModal"
 
 import Navbar from "./Navbar"
 import SubmitForm from "./Forms/SubmitForm"
+import MakeDirectory from "./MakeDirectory"
 
 const HeaderMenu = ()=>{
+  const styleButton = "border-2 rounded-md text-2xl dark:text-white xl:bg-transparent"
+
   return (
     <header className="flex sm:flex-col xl:flex-row mb-4 gap-2 bg-gray-100 items-center dark:bg-gray-900" >
       <Navbar/>
       <OpenModal 
         buttonName="Submit"
-        buttonStyle="text-2xl my-2 py-2 px-4 border-2 rounded-md hover:bg-indigo-500 dark:text-white dark:border-indigo-500"
+        buttonStyle={`${styleButton} my-2 px-4 py-2 hover:bg-indigo-500 dark:border-indigo-500`}
       >
         <SubmitForm />
       </OpenModal> 
+      <OpenModal
+        buttonName="Make Directory"
+        buttonStyle={`${styleButton} mb-3 px-20 py-2 border-emerald-500 bg-emerald-500 hover:bg-emerald-500`}
+      >
+        <MakeDirectory />
+      </OpenModal>
     </header>
   )
 }
